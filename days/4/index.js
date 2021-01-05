45b467ac4a68ce430a7d991f2090c370cd44ba27
Todd McNeill <toddmcneill@gmail.com>
2021-01-05 16:45:02 -0700

    Day 4 part 2

--- days/4/index.js
+++ days/4/index.js
@@ -12,19 +12,64 @@ function parseChunk(chunk) {
   }, {})
 }
 
-function validatePassport(passport) {
-  const requiredFields = [
-    'byr',
-    'iyr',
-    'eyr',
-    'hgt',
-    'hcl',
-    'ecl',
-    'pid',
-  ]
+const requiredFields = [
+  'byr',
+  'iyr',
+  'eyr',
+  'hgt',
+  'hcl',
+  'ecl',
+  'pid',
+]
 
+function validatePassport(passport) {
   return requiredFields.every(field => {
     return field in passport
+  })
+}
+
+function validatePassportWithRules(passport) {
+  const hasAllFields = validatePassport(passport)
+
+  return hasAllFields && requiredFields.every(field => {
+    const val = passport[field]
+    const valAsInt = parseInt(val, 10)
+    switch (field) {
+      case 'byr':
+        return val.length === 4 && valAsInt >= 1920 && valAsInt <= 2002
+      case 'iyr':
+        return val.length === 4 && valAsInt >= 2010 && valAsInt <= 2020
+      case 'eyr':
+        return val.length === 4 && valAsInt >= 2020 && valAsInt <= 2030
+      case 'hgt':
+        const matches = val.match(/^(\d+)(cm|in)$/)
+        if (!matches) {
+          return false
+        }
+        let [_, height, unit] = matches
+        height = parseInt(height, 10)
+        let heightIsValid = true
+        if (unit === 'cm') {
+          heightIsValid = height >= 150 && height <= 193
+        }
+        if (unit === 'in') {
+          heightIsValid = height >= 59 && height <= 76
+        }
+
+        return heightIsValid
+      case 'hcl': {
+        const regex = /^#[\da-f]{6}$/
+        return regex.test(val)
+      }
+      case 'ecl':
+        const validColors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+        return validColors.includes(val)
+      case 'pid': {
+        const regex = /^\d{9}$/
+        return regex.test(val)
+      }
+      default: return true
+    }
   })
 }
 
@@ -40,7 +85,12 @@ function part1(chunks) {
 console.log('part 1 solution: ', part1(chunks))
 
 function part2(chunks) {
-
+  return chunks.map(parseChunk).reduce((acc, cur) => {
+    if (validatePassportWithRules(cur)) {
+      acc ++
+    }
+    return acc
+  }, 0)
 }
 
 console.log('part 2 solution: ', part2(chunks))
